1c922154aec84bfa967f3d6bcbf5e98fc5274b9f
Melissa Perez <perezmelissa053@gmail.com>
2024-10-30 17:25:48 -0400

    Finish update 09

--- 01-node-tutorial/answers/09-path-module.js
+++ 01-node-tutorial/answers/09-path-module.js
@@ -5,3 +5,9 @@ console.log(path.sep)
 const filePath = path.join("/answers", "09-path-module.js")
 
 console.log(filePath)
+
+//const base = path.basename(filePath)
+//console.log(base)
+
+//const absolute = path.resolve(__dirname, "09-path-module.js")
+//console.log(absolute)
