750f49f934b133161e2973f9c16c01a025777b74
Melissa Perez <perezmelissa053@gmail.com>
2024-12-12 23:50:42 -0500

    Commit current work

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        require: [true, "Must provide name."],
+        trim: true,
+        maxlength: [ 20, "Name cannot be longer than 20 characters."]
+
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
