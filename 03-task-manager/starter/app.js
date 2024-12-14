88fbed5d8bcedf52ed5844e70e039981624a7015
Melissa Perez <perezmelissa053@gmail.com>
2024-12-14 15:58:46 -0500

    Finish the basic CRUD controllers

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 //process.env.MONGO_URI = `mongodb+srv://${process.env.USER}:${process.env.PW}@cluster0.jv5qe.mongodb.net/${process.env.COLLECTION_NAME}?retryWrites=true&w=majority&appName=${process.env.APP_NAME}`;
 
-
+app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
