39d26540fc403e808bfdb8eb9d766df927ad9597
Melissa Perez <perezmelissa053@gmail.com>
2024-12-24 00:24:47 -0500

    Add current work

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-
+require("express-async-errors");
 const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
-
+const productsRouter = require("./routes/products");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
     res.send('<h1>Store API</h1> <a href="/api/v1/products">products route</a>');
 });
 
-app.use('/api/v1/products');
+app.use('/api/v1/products', productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
